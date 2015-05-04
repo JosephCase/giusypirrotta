364f82e36ccb442034c5ddfde24b949debb5f9fd
Joseph Case <joseph.case@hotmail.co.uk>
2015-05-04 15:28:26 +0100

    completed R1

--- cv.php
+++ cv.php
@@ -10,9 +10,9 @@
             ?>
 
             <h2>CV</h2>
-            <h2>GIUSY PIRROTTA</h2>
+            <h2 class="name">GIUSY PIRROTTA</h2>
             <h3>REGGIO CALABRIA ITALY 1982<h3>
-            <h3>Live and works in London</h3>
+            <h3>Lives and works in London</h3>
             <h3>pirrottagiusy@gmail.com</h3>
             <h3>+44(0)7866600214</h3>
 
@@ -22,14 +22,14 @@
             <p>Teaching Qualification in Art &amp; Design, Academy of fine art Florence, Italy</p>
             <p>BA in Fine art, Academy of fine art Florence, Italy</p>
 
-            <h4>Awards/Residencies</h4>
+            <h4>AWARDS/RESIDENCIES</h4>
             <p><b>2013</b> Combat Prize, winner video category Giovanni Fattori Museum , Livorno</p>
             <p><b>2012</b> 2012 Red Mansion Art Prize, winner, Red Mansion Art Foundation, London, Residency in Beijing promoted by Red mansion art Foundation after a selection at CSM 2010/2011</p>
             <p><b>2011</b> Nova Award, Cross Over, Lethaby Gallery, London, curated by Lisa Painting, best work produced for Master Fine art 2010/2011</p>
-            <p><b>2008</b> Real Presence 2008, residency award, Belgrade, exhibition at MKM - Magacin/Militar Museum-Kazamati/ Galleria 063 - Castello di Rivoli Turin - curated by Biljana Tomic e Dobrila Denegri nKA / ICA –Belgrade</p>
-            <p><b>2006</b> La fotografia come arte, l’arte come fotografia (The photography as art, art as photography), residency award, Daniel Spoerri Foundation, Grosseto, Italy, Curated by Fiammetta Strigoli,Cassero Senese , Grosseto</p>
+            <p><b>2008</b> Real Presence 2008, residency award, Belgrade, exhibition at MKM - Magacin/Militar Museum-Kazamati/ Galleria 063 - Castello di Rivoli Turin - curated by Biljana Tomic e Dobrila Denegri nKA / ICA - Belgrade</p>
+            <p><b>2006</b> La fotografia come arte, l'arte come fotografia (The photography as art, art as photography), residency award, Daniel Spoerri Foundation, Grosseto, Italy, Curated by Fiammetta Strigoli,Cassero Senese , Grosseto</p>
 
-            <h4>SOLO SHOW</h4>  
+            <h4>SOLO SHOWS</h4>  
             <p><b>2014</b></p>
             <p>Long time ago when the hearth was flat, Galleria Massimo de Luca, curated by Alice Ginaldi, Venice</p>
             <p>The quality of Scale, Murata Centoventidue Contemporary Art, curated by Angela Gonnella, Bari</p>
@@ -41,7 +41,7 @@
             <p>Analog Recurring 2013, Lo &amp; Behold Gallery, curated by Bea Haut, London</p>
             <p>Young at art 2013, Maca Museum Cosenza, curated by Massimo Garofalo and Andrea Rodi, Italy</p>
             <p>Journey to the Center of my mind exhibition, BunkHouse, Madrid curated by Gala Knorr</p>
-            <p>Fama Gallery, Eva’s Daughter curated by Andrea Bruciati, Verona</p>
+            <p>Fama Gallery, Eva's Daughter curated by Andrea Bruciati, Verona</p>
             <p><b>2012</b> Red Mansion Art Prize 2012, Royal Academy of Art, curated by Eliza Bonham Carter e The Red Mansion Foundation</p>
             <p><b>2011</b> Lab.it, Italian Platform for Chinese contemporary art, Beijing,  curated by Cecilia Freschini</p>
             <p>Cross Over, Nova award finalist best work produced for Master in Fine Art Central saint martin 2011 Lethaby Gallery, curated by Lisa painting, London</p>
@@ -49,9 +49,9 @@
             <p>MAFA Final Show, Central Saint Martin College of art &amp; design, London</p>
             <p>Life is elsewhere, Crypt gallery, Curated by Oliver Guy Watkings, London</p>
             
-            <h4>Professional training</h4>
+            <h4>PROFESSIONAL TRAINING</h4>
             <p><b>2013</b></p> 
-            <p>PresTech film laboratories,35mm 16mm film restoration, grading, TK, printing, processing</p>
+            <p>PresTech film laboratories, 35mm 16mm film restoration, grading, TK, printing, processing</p>
             <p><b>2012</b></p>
             <p>16 mm film specialist at Central Saint Martin College of Art &amp; Design Film and video department
 
