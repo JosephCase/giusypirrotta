e96e392a42b340265247d4d0a5fd418081e4ddf5
Joseph Case <joseph@bluequest.co.uk>
2015-06-11 15:15:12 +0100

    working on video functionality

--- js/video.js
+++ js/video.js
@@ -6,24 +6,37 @@
 var Video = new function () {
 	console.log("VIDEO2");
 	this.playVideo = function(video){
-		console.log(video);
-		if (video.mozRequestFullScreen) {
+		console.log("play video");
+		if (video.requestFullscreen) {
+			console.log('normal');
+			video.requestFullscreen();
+		} else if (video.msRequestFullscreen) {
+			console.log('ms');
+			video.msRequestFullscreen();
+			video.setAttribute("controls", "true");
+		} else if (video.mozRequestFullScreen) {
 			console.log('moz');
 			video.mozRequestFullScreen();
 		} else if (video.webkitRequestFullScreen) {
 			console.log('webkit');
 			video.webkitRequestFullScreen();
+		} else {
+			console.log('none worked');
 		}
 		video.play();
 
 		video.addEventListener("mozfullscreenchange",function(){
 			var state = document.fullScreen || document.mozFullScreen || document.webkitIsFullScreen;
-    		console.log(state);
+    		console.log(document.fullScreen);
+    		console.log(document.mozFullScreen);
+    		console.log(document.webkitIsFullScreen);
 		}, false);
 
 		video.addEventListener("webkitfullscreenchange",function(){
 			var state = document.fullScreen || document.mozFullScreen || document.webkitIsFullScreen;
-    		console.log(state);
+    		console.log(document.fullScreen);
+    		console.log(document.mozFullScreen);
+    		console.log(document.webkitIsFullScreen);
 		}, false);
 	}
 };
