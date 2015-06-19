c84ce691d6115abac7760b4c41d778a4aec367ac
Joseph Case <joseph@bluequest.co.uk>
2015-06-19 10:03:02 +0100

    finished video page template

--- moving-image/pagebase.php
+++ moving-image/pagebase.php
@@ -14,32 +14,27 @@
 			<?php 
 				require_once($root."header.php");
             ?>
-            <div class="text">						
-				<h3>Enlighten</h3>
-				<p>
-				    16mm b&amp;w film, loop no sound, 6'45''
-				</p>
-			</div>	
+
+            <?php
+				require_once("contentTitle.php");
+            ?>
+
 			<div class="videoContent">	            	
 				<video loop autoplay poster="image01.jpg">
 					<source src="video.mp4" type="video/mp4">
 					<!-- <source src="video.ogg" type="video/ogg">
 					<source src="video.webm" type="video/webm">-->
 				</video>
-				<img src="image02.png">
-				<img src="image02.png">
-				<img src="image02.png">
+				<?php							
+					$images = glob('image*.jpg');
+					foreach($images as $image) {
+						echo "<img src='".$image."' />";
+					}
+				?>
 			</div>
-			<div class="text">						
-				<p>
-				    The focus of the work is the study of light in and outside the screen, in terms of light shot and emitted between production and postproduction. Light is
-				    shown as an abstracted component in its consistence and ephemerality in the ambiguity between what was shot and what is made with other equipment and in
-				    postproduction.
-				    <br/>
-				    The film is developed and printed by hand. A spotlight is recreated manually frame by frame as if the main character is running toward it. Moreover, the
-				    printer gate was modified in order to recreate a hole which cuts the frame as if a spotlight is enlightening the space.
-				</p>
-			</div>
+			<?php
+				require_once("contentText.php");
+            ?>
 			
 			<?php
 				require_once($root."footer.php");
