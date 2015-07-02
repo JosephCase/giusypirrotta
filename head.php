fe5b5883a515cda6fbdff18147ea3f02aad9f60d
Joseph Case <joseph.case@hotmail.co.uk>
2015-07-02 23:32:17 +0100

    stopped google trying to translate site

--- head.php
+++ head.php
@@ -3,6 +3,7 @@
 
     <!--Meta-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="google" content="notranslate" /> <!-- stops google trying to translate -->
 
     <link rel="shortcut icon" href="favicon.ico">
 
