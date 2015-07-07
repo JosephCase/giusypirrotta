d3264358d7abec6c40e245e17c058b38fbc507cb
Joseph Case <joseph.case@hotmail.co.uk>
2015-07-07 21:18:14 +0100

    fixed ie bug. changed seleted style

--- js/workPage.js
+++ js/workPage.js
@@ -3,12 +3,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
 });
 
 function workPage() {
-	var aImages = document.getElementsByClassName('images')[0].getElementsByTagName('img');
+	var aImages = document.getElementsByTagName('img');
 	for (var i = 0; i < aImages.length; i++) {
+		// aImages[i].className = this.className + ' loaded';
 		aImages[i].addEventListener("load", function() {
-			console.log(this);
 			this.className = this.className + ' loaded';
-		});
+		}, false);
+
+		aImages[i].src = aImages[i].src;
 	};
 	// $('images img').on('load', function() {
 	// 	this.addClass("loaded");
