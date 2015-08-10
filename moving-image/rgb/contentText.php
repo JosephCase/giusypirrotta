954e0cf76b43f7e1e7efc768a748ed367b696cd1
Joseph Case <joseph@bluequest.co.uk>
2015-08-10 11:30:35 +0100

    added some text

--- moving-image/rgb/contentText.php
+++ moving-image/rgb/contentText.php
@@ -1,4 +1,4 @@
 <div class="text">						
-	<p class="english">###</p>
-	<p class="italian">###</p>
+	<p>The Film shows Flower and gardens views from books assembled through a sculptural college and illuminated under red Blue and Green Light.<br />The original colour of the pictures are altered while images of an open landscape plays on the ambiguity of what it is perceived between reality and fiction.</p>
+	<!-- <p class="italian">###</p> -->
 </div>
