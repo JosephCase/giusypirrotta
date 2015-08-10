b7623b0f7ba63e4132f0fd571146c4c50a28e061
Joseph Case <joseph@bluequest.co.uk>
2015-08-10 16:10:35 +0100

    added vanishpoint title #1

--- moving-image/vanishing-point/contentTitle.php
+++ moving-image/vanishing-point/contentTitle.php
@@ -1,4 +1,4 @@
 <div class="title">
 	<h3>Vanishing Point</h3>				
-	<p>3 channel video projection on pvc, black paper, wood structure , 4'45'', loop, no sound, 2011/12<br />16mm B&amp;W film 10'39'' projected on pvc and black paper, loop, 2012<br />installation view at Royal Academy fo Art, London, UK<br />installation view at Placentia art gallery, Piacenza, Italy</p>
+	<p>Video part of the installation <a href="/work/vanishing_point">Vanishing Point</a>, central projection of the 3 channels video projection, 4&rsquo;45&rsquo;&rsquo;, loop, no sound, 2012</p>
 </div>	
