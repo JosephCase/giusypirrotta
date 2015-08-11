0860b6549069b87542db11adb7c064a97e324731
Joseph Case <joseph@bluequest.co.uk>
2015-08-11 11:01:20 +0100

    added missing content

--- moving-image/enlighten/contentTitle.php
+++ moving-image/enlighten/contentTitle.php
@@ -1,4 +1,4 @@
 <div class="title">
 	<h3>Enlighten</h3>				
-	<p>16mm b&amp;w film, loop no sound, 6&rsquo;45&rsquo;&rsquo;</p>
+	<p>16mm b&amp;w film, loop no sound, 6&rsquo;45&rsquo;&rsquo;</br>projection part of the installation <a href="/work/enlighten">Enlighten</a></p>
 </div>	
