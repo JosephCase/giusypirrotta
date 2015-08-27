253111d7099b8852b40ad81bc2cd8fc06009c31e
Joseph Case <joseph@bluequest.co.uk>
2015-08-27 16:07:59 +0100

    fixed text bug

--- moving-image/chroma/contentText.php
+++ moving-image/chroma/contentText.php
@@ -1,4 +1,4 @@
 <div class="text">						
 	<p class="english">The film plays on the ambiguity between reality and fiction in terms of the cinematographic aspect of what can be perceived and subvert throughout film making and the space of production.<br />The film studio , the green screen as well as lights and actors are outlined order to create a surreal space which is unveiled by the developing of the film</p>
-	<p class="itlaian">Il film studia l&rsquo;ambiguit&agrave; della messa in scena tra reale e cinematografico, tra quello che lo spettatore percepisce e come questo &egrave; modificato attraverso i mezzi di produzione e nello spazio di produzione e fruizione dell&rsquo;opera.</p>
+	<p class="italian">Il film studia l&rsquo;ambiguit&agrave; della messa in scena tra reale e cinematografico, tra quello che lo spettatore percepisce e come questo &egrave; modificato attraverso i mezzi di produzione e nello spazio di produzione e fruizione dell&rsquo;opera.</p>
 </div>
